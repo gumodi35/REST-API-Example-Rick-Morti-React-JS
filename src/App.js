624519f38246a5061b6250e9c40edacc5403132c
Gualberto <gumodi10@gmail.com>
2022-11-29 14:51:15 -0600

    Add Hook to update state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
-//import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 //import  { Card } from "./components/Card";
 //import { Filter } from "./components/Filter";
 //import { Navbar } from "./components/Navbar";
@@ -8,6 +8,20 @@ import "bootstrap/dist/js/bootstrap";
 //import { Search } from "./components/Search";
 
 function App() {
+
+  const [apidata, setapidate] = useState([]);
+  const { infodata, resultsdata } = apidata;
+  const api = `https://rickandmortyapi.com/api/character/?page=2`;
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetch(api)
+      .then((res) => 
+      res.json());
+      setapidate(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
      <h1 className="text-center mb-3">Personajes</h1>
